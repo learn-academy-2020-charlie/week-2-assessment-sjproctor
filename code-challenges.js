686e52d14e9e21af9722936a090f2d2f4a42214c
sjproctor <sproctor950@gmail.com>
2020-06-28 13:48:28 -0700

    WIP

---
+++ code-challenges.js
@@ -0,0 +1,10 @@
+// --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
+// Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
+
+
+var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+
+
+
+// --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
+// Expected output: "Itchy" "Daffy" "Ren"
